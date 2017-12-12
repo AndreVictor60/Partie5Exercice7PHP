20520cdabd758b7d5439cc426f45bc943fe6a926
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:32:43 +0100

    fini

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 5 Exercice 7</title>
+  </head>
+  <body>
+    <?php
+      $hautDeFrance = array(60=>'Oise',80=>'Somme',02=>'Aisne',62=>'Pas-de-calais',59=>'Nord');
+      $hautDeFrance[51] = 'Marne';
+      print_r($hautDeFrance);
+     ?>
+  </body>
+</html>
